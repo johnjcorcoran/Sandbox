0cc497adbcbebd9e943ef9381d6253fa0bcd17b6
JohnJCorcoran <jcyorkshire@gmail.com>
2013-08-18 20:58:39 +0100

    Fixed formatting

--- Sandbox/Sandbox/StringCalculatorKata/StringCalculator.cs
+++ Sandbox/Sandbox/StringCalculatorKata/StringCalculator.cs
@@ -4,104 +4,105 @@ using System.Linq;
 
 namespace Sandbox.StringCalculatorKata
 {
-	public class StringCalculator
-	{
-		private const int ResultIfInputEmpty = 0;
-		private const int MaximumLegalInput = 1000;
-		private const string newLineSymbol = "\n";
-		private const string CustomDelimiterStart = "[";
-		private const string CustomDelimiterEnd = "]";
-		private const string CustomDelimiterIndicator = "//";
-		private static readonly List<string> Delimiters = new List<string>{",", newLineSymbol};
+    public class StringCalculator
+    {
+        private const int ResultIfInputEmpty = 0;
+        private const int MaximumLegalInput = 1000;
+        private const string newLineSymbol = "\n";
+        private const string CustomDelimiterStart = "[";
+        private const string CustomDelimiterEnd = "]";
+        private const string CustomDelimiterIndicator = "//";
+        private static readonly List<string> Delimiters = new List<string>{",", newLineSymbol};
 
-		public int Add (string numbers)
-		{
-			if (string.IsNullOrEmpty (numbers)) {
-				return ResultIfInputEmpty;
-			}
+        public int Add (string numbers)
+        {
+            if (string.IsNullOrEmpty (numbers)) {
+                return ResultIfInputEmpty;
+            }
 
-			return CustomDelimiterUsed(numbers) 
-				? ParseWithCustomDelimiters(numbers).Sum()
-				: Parse(numbers).Sum();
-		}
+            return CustomDelimiterUsed (numbers) 
+                ? ParseWithCustomDelimiters (numbers).Sum ()
+                : Parse (numbers).Sum ();
+        }
 
-		private int[] ParseWithCustomDelimiters (string numbers)
-		{
-			ExtractCustomDelimitersFrom (FirstLineOf (numbers));
-			return Parse (SecondLineOnwardsOf (numbers));
-		}
+        private int[] ParseWithCustomDelimiters (string numbers)
+        {
+            ExtractCustomDelimitersFrom (FirstLineOf (numbers));
+            return Parse (SecondLineOnwardsOf (numbers));
+        }
 
-		private void ExtractCustomDelimitersFrom (string numbers)
-		{
-			if (LongDelimiterUsed (numbers)) {
-				ExtractLongCustomDelimiters (numbers);
-			} else {
-				AddSingleCharacterCustomDelimiter (numbers);
-			}
-		}
+        private void ExtractCustomDelimitersFrom (string numbers)
+        {
+            if (LongDelimiterUsed (numbers)) {
+                ExtractLongCustomDelimiters (numbers);
+            } else {
+                AddSingleCharacterCustomDelimiter (numbers);
+            }
+        }
 
-	    private int[] Parse (string numbers)
-		{
-			int[] integers = numbers.Split(Delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
-						            .Select(n => Convert.ToInt32(n))
-									.Where(n => n <= MaximumLegalInput)
-						            .ToArray();
-			ThrowExceptionIfAnyNegative(integers);
-			return integers;
-		}
+        private int[] Parse (string numbers)
+        {
+            int[] integers = numbers.Split (Delimiters.ToArray (), StringSplitOptions.RemoveEmptyEntries)
+                                    .Select (n => Convert.ToInt32 (n))
+                                    .Where (n => n <= MaximumLegalInput)
+                                    .ToArray ();
+            ThrowExceptionIfAnyNegative (integers);
+            return integers;
+        }
 
-		private void ExtractLongCustomDelimiters (string numbers)
-		{
-			string[] delimiters = numbers.Remove (0, 2)
-				       					 .Split (CustomDelimiterEnd.ToArray (), StringSplitOptions.RemoveEmptyEntries);
-			foreach (string delimiter in delimiters) {
-				Delimiters.Add(delimiter.Remove (0, 1));
-			}
-		}
+        private void ExtractLongCustomDelimiters (string numbers)
+        {
+            string[] delimiters = numbers.Remove (0, 2)
+                                            .Split (CustomDelimiterEnd.ToArray (), StringSplitOptions.RemoveEmptyEntries);
+            foreach (string delimiter in delimiters) {
+                Delimiters.Add (delimiter.Remove (0, 1));
+            }
+        }
 
-		private void ThrowExceptionIfAnyNegative (int[] integers)
-		{
-			if (!integers.Any (x => x < 0)) return;
+        private void ThrowExceptionIfAnyNegative (int[] integers)
+        {
+            if (!integers.Any (x => x < 0))
+                return;
 
-			int[] negatives = integers.Where(x => x < 0).ToArray();
-			throw new Exception(String.Format(
-				"negatives not allowed - {0}", FormatNumbers(negatives)));
-		}
+            int[] negatives = integers.Where (x => x < 0).ToArray ();
+            throw new Exception (String.Format (
+                "negatives not allowed - {0}", FormatNumbers (negatives))
+            );
+        }   
 
-		private string FormatNumbers (int[] negatives)
-		{
-			return string.Join (" ", negatives);
-		}
+        private string FormatNumbers (int[] negatives)
+        {
+            return string.Join (" ", negatives);
+        }
 
-		private void AddSingleCharacterCustomDelimiter (string numbers)
-		{
-			Delimiters.Add (numbers.Substring (2, 1));
-		}
+        private void AddSingleCharacterCustomDelimiter (string numbers)
+        {
+            Delimiters.Add (numbers.Substring (2, 1));
+        }
 
-		private static bool CustomDelimiterUsed (string numbers)
-		{
-			return numbers.StartsWith (CustomDelimiterIndicator);
-		}
+        private static bool CustomDelimiterUsed (string numbers)
+        {
+            return numbers.StartsWith (CustomDelimiterIndicator);
+        }
 
-		private static bool LongDelimiterUsed (string numbers)
-		{
-			return numbers.Contains (CustomDelimiterStart);
-		}
+        private static bool LongDelimiterUsed (string numbers)
+        {
+            return numbers.Contains (CustomDelimiterStart);
+        }
 
-		private static string FirstLineOf (string numbers)
-		{
-			return numbers.Substring(0, GetSecondLineIndex (numbers));
-		}
+        private static string FirstLineOf (string numbers)
+        {
+            return numbers.Substring (0, GetSecondLineIndex (numbers));
+        }
 
-		private static string SecondLineOnwardsOf (string numbers)
-		{
-			return numbers.Substring (GetSecondLineIndex (numbers)+1);
-		}
+        private static string SecondLineOnwardsOf (string numbers)
+        {
+            return numbers.Substring (GetSecondLineIndex (numbers) + 1);
+        }
 
-		static int GetSecondLineIndex (string numbers)
-		{
-			return numbers.IndexOf (newLineSymbol);
-		}
-	}
+        static int GetSecondLineIndex (string numbers)
+        {
+            return numbers.IndexOf (newLineSymbol);
+        }
+    }
 }
-
--- Sandbox/Sandbox/StringCalculatorKataTests/StringCalculatorTests.cs
+++ Sandbox/Sandbox/StringCalculatorKataTests/StringCalculatorTests.cs
@@ -5,73 +5,72 @@ using Sandbox.StringCalculatorKata;
 
 namespace Sandbox.StringCalculatorKataTests
 {
-	[TestFixture]
-	public class StringCalculatorTests
-	{
-		private StringCalculator _calculator;
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        private StringCalculator _calculator;
 
-		[SetUp]
-		public void SetUp()
-		{
-			_calculator = new StringCalculator();
-		}
+        [SetUp]
+        public void SetUp ()
+        {
+            _calculator = new StringCalculator ();
+        }
 
-		[Test]
-		public void AddWithEmptyStringReturnsZero()
-		{
-			Assert.That(_calculator.Add(""), Is.EqualTo(0));
-		}
+        [Test]
+        public void AddWithEmptyStringReturnsZero ()
+        {
+            Assert.That (_calculator.Add (""), Is.EqualTo (0));
+        }
 
-		[TestCase("1", 1)]
-		[TestCase("2", 2)]
-		public void AddingSingleNumberReturnsThatNumber (string numbers, int result)
-		{
-			Assert.That(_calculator.Add(numbers), Is.EqualTo(result));
-		}
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void AddingSingleNumberReturnsThatNumber (string numbers, int result)
+        {
+            Assert.That (_calculator.Add (numbers), Is.EqualTo (result));
+        }
 
-		[TestCase("1,2", 3)]
-		[TestCase("2,3", 5)]
-		[TestCase("1,2,3,4,5", 15)]
-		public void AddingMultipleDelimitedNumbersReturnsSumOfNumbers(string numbers, int result)
-		{
-			Assert.That(_calculator.Add(numbers), Is.EqualTo(result));
-		}
+        [TestCase("1,2", 3)]
+        [TestCase("2,3", 5)]
+        [TestCase("1,2,3,4,5", 15)]
+        public void AddingMultipleDelimitedNumbersReturnsSumOfNumbers (string numbers, int result)
+        {
+            Assert.That (_calculator.Add (numbers), Is.EqualTo (result));
+        }
 
-		[TestCase("1\n2", 3)]
-		[TestCase("1\n2,3", 6)]
-		public void AddingMultipleNewLineDelimitedNumbersReturnsSumOfNumbers(string numbers, int result)
-		{
-			Assert.That(_calculator.Add(numbers), Is.EqualTo(result));
-		}
+        [TestCase("1\n2", 3)]
+        [TestCase("1\n2,3", 6)]
+        public void AddingMultipleNewLineDelimitedNumbersReturnsSumOfNumbers (string numbers, int result)
+        {
+            Assert.That (_calculator.Add (numbers), Is.EqualTo (result));
+        }
 
-		[TestCase("//;\n1;2", 3)]
-		[TestCase("//;\n1;2\n3", 6)]
-		public void AddAllowsCustomDelimiter(string numbers, int result)
-		{
-			Assert.That(_calculator.Add(numbers), Is.EqualTo(result));
-		}
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//;\n1;2\n3", 6)]
+        public void AddAllowsCustomDelimiter (string numbers, int result)
+        {
+            Assert.That (_calculator.Add (numbers), Is.EqualTo (result));
+        }
 
-		[TestCase("-1", "-1")]
-		[TestCase("-1, 2, -3", "-1 -3")]
-		public void AddThrowsExceptionGivenNegativeNumbers(string numbers, string expected)
-		{
-			Assert.Throws(Is.TypeOf<Exception>().And.Message.EqualTo("negatives not allowed - " + expected), 
-			              () => _calculator.Add(numbers));
-		}
+        [TestCase("-1", "-1")]
+        [TestCase("-1, 2, -3", "-1 -3")]
+        public void AddThrowsExceptionGivenNegativeNumbers (string numbers, string expected)
+        {
+            Assert.Throws (Is.TypeOf<Exception> ().And.Message.EqualTo ("negatives not allowed - " + expected), 
+                       () => _calculator.Add (numbers));
+        }
 
-		[TestCase("1,1001", 1)]
-		[TestCase("1,1000,1001", 1001)]
-		public void AddIgnoresNumbersGreaterThan1000(string numbers, int result)
-		{
-			Assert.That(_calculator.Add(numbers), Is.EqualTo(result));
-		}
+        [TestCase("1,1001", 1)]
+        [TestCase("1,1000,1001", 1001)]
+        public void AddIgnoresNumbersGreaterThan1000 (string numbers, int result)
+        {
+            Assert.That (_calculator.Add (numbers), Is.EqualTo (result));
+        }
 
-		[TestCase("//[***]\n1***2", 3)]
-		[TestCase("//[***][!!!]\n1***2!!!3", 6)]
-		public void AddAllowsMultipleCustomDelimitersOfAnyLength(string numbers, int result)
-		{
-			Assert.That(_calculator.Add(numbers), Is.EqualTo(result));
-		}
-	}
+        [TestCase("//[***]\n1***2", 3)]
+        [TestCase("//[***][!!!]\n1***2!!!3", 6)]
+        public void AddAllowsMultipleCustomDelimitersOfAnyLength (string numbers, int result)
+        {
+            Assert.That (_calculator.Add (numbers), Is.EqualTo (result));
+        }
+    }
 }
-
